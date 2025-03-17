b68162b51d479dc3bb786745eb7aee048562c915
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-03-17 23:45:22 +0000

    almost compelte

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,146 @@
-public class PracticeProblem {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
 
+public class PracticeProblem {
 	public static void main(String args[]) {
+
+		FileWriter fileWriter = null;
+
+		try {
+			fileWriter = new FileWriter("file");
+			fileWriter.write("Jacob Park 17 123456789\n");
+			fileWriter.write("Eric Lan 17 987654321\n");
+			fileWriter.write("Tiffany Chiu 17 123654789");
+		}
+		catch (IOException e) {}
+		finally {
+			try {
+				if (fileWriter != null) {
+					fileWriter.close();
+				}
+			}
+			catch (IOException e) {}
+		}
+
+		System.out.print(getAge(2, "file"));
+	}
+
+	public static String getName(int Line, String file) {
 		
+		BufferedReader inputStream = null;
+
+		try {
+			inputStream = new BufferedReader(new FileReader(file));
+
+			String line = "";
+				for(int i=1;i<=Line;i++) {
+					line = inputStream.readLine();
+					if (line == null) {
+						return ""; 
+					}
+				}
+			
+			String[] nameArr = line.split(" ");
+			String name = nameArr[0] + " " + nameArr[1];
+			return name;
+		}
+
+		catch (IOException e) {return "";}
+		
+		finally {
+		 	try {
+				if (inputStream != null) {
+		 			inputStream.close();
+		 		}
+		 	}
+		 	catch (IOException e) {}
+		 }
+	}
+
+	public static String getAge(int Line, String file) {
+		
+		BufferedReader inputStream = null;
+
+		try {
+			inputStream = new BufferedReader(new FileReader(file));
+
+			String line = "";
+				for(int i=1;i<=Line;i++) {
+					line = inputStream.readLine();
+					if (line == null) {
+						return "-1"; 
+					}
+				}
+			
+			String[] ageArr = line.split(" ");
+			String age = ageArr[2];
+			return age;
+		}
+
+		catch (IOException e) {return "-1";}
+		
+		finally {
+		 	try {
+				if (inputStream != null) {
+		 			inputStream.close();
+		 		}
+		 	}
+		 	catch (IOException e) {}
+		 }
+	}
+
+	public static String getNumber(int Line, String file) {
+		
+		BufferedReader inputStream = null;
+
+		try {
+			inputStream = new BufferedReader(new FileReader(file));
+
+			String line = "";
+				for(int i=1;i<=Line;i++) {
+					line = inputStream.readLine();
+					if (line == null) {
+						return "-1"; 
+					}
+				}
+			
+			String[] numArr = line.split(" ");
+			String num = numArr[3];
+			return num;
+		}
+
+		catch (IOException e) {return "-1";}
+		
+		finally {
+		 	try {
+				if (inputStream != null) {
+		 			inputStream.close();
+		 		}
+		 	}
+		 	catch (IOException e) {}
+		 }
+	}
+
+	public static void fileAppend(String output, String filename) {
+		BufferedWriter bw = null;
+
+		try {
+			bw = new BufferedWriter(new FileWriter(filename, true));
+			bw.write(output);
+		}
+		
+		catch(IOException e) {}
+		
+		finally {
+			try {
+				if (bw != null) {
+					bw.close();
+				}
+			}
+			catch (IOException e) {}
+		}
 	}
 }
